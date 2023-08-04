e41b032b0c0ea278acb85123a847ea366f749396
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-08-04 08:42:31 +0200

    fix apply patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-patch -N -b -u /usr/share/awesome/lib/awful/widget/layoutbox.lua -i patches/layoutbox_text_support.patch
-patch -N -b -u /usr/share/awesome/lib/beautiful/theme_assets.lua -i patches/taglist_squares_unsel_thickness.patch
-patch -N -b -u /usr/share/awesome/lib/awful/widget/taglist.lua -i patches/taglist_squares_urgent.patch
+patch -N -b -u /usr/share/awesome/lib/beautiful/theme_assets.lua -i taglist_squares_unsel_thickness.patch
+patch -N -b -u /usr/share/awesome/lib/awful/widget/taglist.lua -i taglist_squares_urgent.patch
