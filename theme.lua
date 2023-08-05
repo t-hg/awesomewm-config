d816428d9d926c7359336f92c0ac5c3274974534
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-08-05 11:33:24 +0200

    theme

--- theme.lua
+++ theme.lua
@@ -1,7 +1,7 @@
 local gears = require("gears")
 local beautiful = require("beautiful")
 
-local theme_name = "default"
+local theme_name = "dracula"
 local theme = gears.filesystem.get_configuration_dir() .. "themes/" .. theme_name .. "/theme.lua"
 
 local function init()
