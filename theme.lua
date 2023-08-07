881bff76eb6069fe61a9fd70c202d00636a316d7
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-08-07 21:12:30 +0200

    set wallpaper

--- theme.lua
+++ theme.lua
@@ -4,8 +4,11 @@ local beautiful = require("beautiful")
 local theme_name = "dracula"
 local theme = gears.filesystem.get_configuration_dir() .. "themes/" .. theme_name .. "/theme.lua"
 
+local wallpaper = "~/Pictures/wallpaper.jpg"
+
 local function init()
   beautiful.init(theme)
+  gears.wallpaper.maximized(wallpaper, nil)
 end
 
 return {
